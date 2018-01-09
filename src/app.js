a7cf1a14dc48cd9357a96b3637a402a3288ab2d9
Anna Warzecha <anna.warzecha@gmail.com>
2018-01-09 23:45:22 +0100

    Add all encodings to the encodings selector

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 // see http://stackoverflow.com/questions/2897619/using-html5-javascript-to-generate-and-save-a-file
 // see http://stackoverflow.com/questions/18662404/download-lengthy-data-as-a-csv-file
+var encodings = [
+  "UTF-8", "IBM866", "ISO-8859-2", "ISO-8859-3", "ISO-8859-4", "ISO-8859-5",
+  "ISO-8859-6", "ISO-8859-7", "ISO-8859-8", "ISO-8859-8-I", "ISO-8859-10",
+  "ISO-8859-13", "ISO-8859-14", "ISO-8859-15", "ISO-8859-16", "KOI8-R",
+  "KOI8-U", "macintosh", "windows-874", "windows-1250", "windows-1251",
+  "windows-1252", "windows-1253", "windows-1254", "windows-1255",
+  "windows-1256", "windows-1257", "windows-1258", "x-mac-cyrillic", "GBK",
+  "gb18030", "Big5", "EUC-JP", "ISO-2022-JP", "Shift_JIS", "EUC-KR",
+  "replacement", "UTF-16BE", "UTF-16LE", "x-user-defined"
+]
+
 angular.element(document).ready(function() {
   angular.module("app", []);
   angular.module("app").directive("fileread", [
@@ -78,7 +89,7 @@ angular.element(document).ready(function() {
       Outflow: "Amount"
     };
     $scope.file = {
-      encodings: ["UTF-8", "ISO-8859-1", "windows-1250"],
+      encodings: encodings,
       chosenEncoding: "UTF-8"
     };
     $scope.data_object = new DataObject();
